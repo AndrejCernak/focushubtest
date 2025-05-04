8e5e3771d9894f878cbcaa4fbb35498faefdaf7b
AndrejCernak <165067032+AndrejCernak@users.noreply.github.com>
2025-05-04 21:00:17 +0200

    Create install.py

---
+++ focus_hub/install.py
@@ -0,0 +1,10 @@
+json_files = [
+    "faktury_workspace.json",
+    "vystavene_faktury_workspace.json",
+    "prijate_faktury_workspace.json",
+    "vydavky_workspace.json",
+    "clenovia_workspace.json"
+]
+
+for file in json_files:
+    frappe.reload_doc("focus_hub", "config", file.replace(".json", ""))
