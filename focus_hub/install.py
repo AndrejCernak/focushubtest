3770d2c9c4bf2dcd5c4950043c9918dc5c2cc256
AndrejCernak <165067032+AndrejCernak@users.noreply.github.com>
2025-05-04 21:04:16 +0200

    Update install.py

--- focus_hub/install.py
+++ focus_hub/install.py
@@ -1,10 +1,13 @@
-json_files = [
-    "faktury_workspace.json",
-    "vystavene_faktury_workspace.json",
-    "prijate_faktury_workspace.json",
-    "vydavky_workspace.json",
-    "clenovia_workspace.json"
-]
+import frappe
 
-for file in json_files:
-    frappe.reload_doc("focus_hub", "config", file.replace(".json", ""))
+def load_workspaces():
+    json_files = [
+        "faktury_workspace.json",
+        "vystavene_faktury_workspace.json",
+        "prijate_faktury_workspace.json",
+        "vydavky_workspace.json",
+        "clenovia_workspace.json"
+    ]
+
+    for file in json_files:
+        frappe.reload_doc("focus_hub", "workspace", file.replace(".json", ""))
